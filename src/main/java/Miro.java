260edc9b93382962e898d5101fd85edd1eb584fc
Jun Wei <156562637+junwei07@users.noreply.github.com>
2025-08-17 21:30:48 +0800

    Add echo commands functionality

--- src/main/java/Miro.java
+++ src/main/java/Miro.java
@@ -1,28 +1,47 @@
+import java.util.Scanner;
+
 public class Miro {
     private boolean isRunning;
+    private Scanner sc = new Scanner(System.in);
 
     public Miro() {
         this.isRunning = true;
     }
 
     private void Hbar() {
+        space();
         System.out.println("───────────────────────────────");
     }
     private void run() {
         Hbar();
         greet();
         while (isRunning) {
+            String input = sc.nextLine().strip().toLowerCase();
+            if (input.equals("bye")) {
+                break;
+            }
+            Hbar();
+            space();
+            System.out.printf("%s\n", input);
+            Hbar();
         }
         exit();
     }
 
+    private void space() {
+        System.out.print("    ");
+    }
+
     private void greet() {
-        System.out.println("Hello I'm Miro\nWhat can I do for you?\n");
+        space();
+        System.out.println("Hello I'm Miro\n");
+        space();
+        System.out.println("What can I do for you?\n");
         Hbar();
-        this.isRunning = false;
     };
 
     private void exit() {
+        space();
         System.out.println("GoodBye. Hope to See you again!\n");
         Hbar();
     }
@@ -34,6 +53,6 @@ public class Miro {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I'm Miro\nWhat can I do for you?");
+//        System.out.println("Hello I'm Miro\nWhat can I do for you?");
     }
 }
